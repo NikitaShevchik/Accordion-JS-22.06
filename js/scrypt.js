a03919d989c899603f3a2e923d7e52e84a66dcdc
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-22 17:21:19 +0300

    js+css+html

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,1 +1,11 @@
 "use strict"
+
+const accordHeader = document.querySelectorAll('.accord__header');
+
+
+for (let ac of accordHeader){
+    ac.addEventListener('click', function(){
+        ac.classList.toggle('_open');
+        ac.parentNode.classList.toggle('_active')
+    })
+}
